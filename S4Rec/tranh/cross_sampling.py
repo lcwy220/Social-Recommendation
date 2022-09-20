16e1fc3371402d98950d60a65c2fea01418693a5
lcwy220 <yuankun@buaa.edu.cn>
2022-09-20 21:18:39 +0800

    Add files via upload

---
+++ S4Rec/tranh/cross_sampling.py
@@ -0,0 +1,110 @@
+import pickle
+import random
+import sys
+import os
+from collections import defaultdict
+
+# you need to change the dataset_path for different data
+dataset_name = 'Ciao'
+dataset_path = '../datasets/'+dataset_name+'/'
+
+if not os.path.exists(dataset_name):
+    os.mkdir(dataset_name)
+
+with open(dataset_path + 'dataset_filter5.pkl', 'rb') as f:
+    train_set = pickle.load(f)
+    valid_set = pickle.load(f)
+    test_set = pickle.load(f)
+
+with open(dataset_path + 'list_filter5.pkl', 'rb') as f:
+    u_items_list = pickle.load(f)
+    u_users_list = pickle.load(f)
+    u_users_items_list = pickle.load(f)
+    i_users_list = pickle.load(f)
+    (user_count, item_count, rate_count) = pickle.load(f)
+
+
+u_dict = defaultdict(list)
+i_dict = defaultdict(list)
+
+
+for u,i, r in train_set:
+    if r == 0:
+        continue
+    u_dict[str(u)+'_'+str(r)].append(i)
+    i_dict[str(i)+'_'+str(r)].append(u)
+
+
+
+
+neg_train_list = []
+pos_train_list = []
+all_user_set = set(range(1, user_count+1))
+all_item_set = set(range(1, item_count+1))
+
+count = 0
+new_train_set = []
+pos_new_train_list = []
+
+for u,i, r in train_set:
+    if r == 0:
+        continue
+    new_train_set.append((u,i,r)) #
+
+    tmp_list = []
+    # specify to 1
+    if r == 1:
+        for idx in [2,3,4,5]:
+            item_set = set(u_dict[str(u)+'_'+str(idx)])
+            for k in item_set:
+                pos_train_list.append((u,i,1))
+                neg_train_list.append((u,k,1))
+                count += 1
+                if count % 1000 == 0:
+                    print(count)
+
+            user_set = set(i_dict[str(i)+'_'+str(idx)])
+            for k in user_set:
+                pos_train_list.append((u,i,1))
+                neg_train_list.append((k,i,1))
+                tmp_list.append((k,i,1))
+                count += 1
+                if count % 1000 == 0:
+                    print(count)
+
+
+    for j in range(1,r):
+        item_set = set(u_dict[str(u)+'_'+str(j)])
+        for k in item_set:
+            pos_train_list.append((u,i,r))
+            neg_train_list.append((u,k,r))
+            tmp_list.append((u,k,r))
+            count += 1
+            if count % 1000 == 0:
+                print(count)
+            
+        user_set = set(i_dict[str(i)+'_'+str(j)])
+        for k in user_set:
+            pos_train_list.append((u,i,r))
+            neg_train_list.append((k,i,r))
+            tmp_list.append((k,i,r))
+            count += 1
+            if count % 1000 == 0:
+                print(count)
+        
+    neg_tmp_list = random.sample(tmp_list, min(30, len(tmp_list)))
+    pos_new_train_list.append(neg_tmp_list)
+
+print(len(new_train_set))
+print(len(pos_train_list), len(neg_train_list))
+
+
+with open('%s/new_train_set_transh_30.pkl' %dataset_name, 'wb') as f:
+    pickle.dump(new_train_set, f)
+    pickle.dump(pos_new_train_list, f)
+
+with open('%s/relation_train_set.pkl' %dataset_name, 'wb') as f:
+    pickle.dump(pos_train_list, f)
+    pickle.dump(neg_train_list, f)
+
+
---
+++ S4Rec/tranh/tranh.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class TransH(nn.Module):
+    def __init__(self, hentityemb, tentityemb, HentityNum, TentityNum, relationNum=5, embeddingDim=64, margin=1.0, L=2, C=1.0, eps=0.001):
+        super(TransH, self).__init__()
+        assert (L in [1, 2])
+        self.model = "TransH"
+        self.Hentnum = HentityNum
+        self.Tentnum = TentityNum
+        self.relnum = relationNum
+        self.margin = margin
+        self.L = L
+        self.C = C
+        self.eps = eps
+
+        self.HentityEmbedding = hentityemb
+        self.TentityEmbedding = tentityemb
+
+        self.relationEmbedding = nn.Embedding(num_embeddings=relationNum+1,
+                                            embedding_dim=embeddingDim)
+
+        self.relationHyper = nn.Embedding(num_embeddings=relationNum+1,
+                                          embedding_dim=embeddingDim)
+        self.distfn = nn.PairwiseDistance(L)
+
+    '''
+    Calculate the score:
+    Step1 : Split the triple as head, relation and tail
+    Step2 : Transform index tensor to embedding tensor
+    Step3 : Project entity head and tail embedding to relation hyperplane
+    Step4 : Calculate similarity score in relation hyperplane
+    Step5 : Return the score
+    '''
+    def scoreOp(self, inputTriple):
+        # Step1
+        head, tail, relation = torch.chunk(inputTriple,
+                                           chunks=3,
+                                           dim=1)
+        # Step2
+        head = torch.squeeze(self.HentityEmbedding(head), dim=1)
+        relHyper = torch.squeeze(self.relationHyper(relation), dim=1)
+        relation = torch.squeeze(self.relationEmbedding(relation), dim=1)
+        tail = torch.squeeze(self.TentityEmbedding(tail), dim=1)
+        # Step3
+        head = head - relHyper * torch.sum(head * relHyper, dim=1, keepdim=True)
+        tail = tail - relHyper * torch.sum(tail * relHyper, dim=1, keepdim=True)
+        # Step4
+        return self.distfn(head+relation, tail)
+
+    '''
+    Normalize relation hyper-plane embedding
+    '''
+    def normalizeEmbedding(self):
+        hyperWeight = self.relationHyper.weight.detach().cpu().numpy()
+        hyperWeight = hyperWeight / np.sqrt(np.sum(np.square(hyperWeight), axis=1, keepdims=True))
+        self.relationHyper.weight.data.copy_(torch.from_numpy(hyperWeight))
+
+    def retEvalWeights(self):
+        return {"HentityEmbed": self.HentityEmbedding.weight.detach().cpu().numpy(),
+                "TentityEmbed": self.TentityEmbedding.weight.detach().cpu().numpy(),
+                "relationEmbed": self.relationEmbedding.weight.detach().cpu().numpy(),
+                "hyperEmbed": self.relationHyper.weight.detach().cpu().numpy()}
+
+    def forward(self, posX, negX):
+        size = posX.size()[0]
+        # Calculate score
+        posScore = self.scoreOp(posX)
+        negScore = self.scoreOp(negX)
+
+        # Get margin ranking loss
+        # max(posScore-negScore+margin, 0)
+        # Use F.relu()
+        marginLoss = torch.sum(F.relu(input=posScore-negScore+self.margin))
+        HentityLoss = torch.sum(F.relu(torch.norm(self.HentityEmbedding.weight, p=2, dim=1, keepdim=False)-1))
+        TentityLoss = torch.sum(F.relu(torch.norm(self.TentityEmbedding.weight, p=2, dim=1, keepdim=False)-1))
+        orthLoss = torch.sum(F.relu(torch.sum(self.relationHyper.weight * self.relationEmbedding.weight, dim=1, keepdim=False) / \
+                                    torch.norm(self.relationEmbedding.weight, p=2, dim=1, keepdim=False) - self.eps ** 2))
+        return  marginLoss/size +self.C*(HentityLoss/self.Hentnum + TentityLoss/self.Tentnum + orthLoss/self.relnum)
